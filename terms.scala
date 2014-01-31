a6fce1c1dbbd5b18cd93059cc27d8a7c486e9e2e
Saroupille <franth2@gmail.com>
2014-01-31 17:32:12 +0100

    Add TermValue

--- terms.scala
+++ terms.scala
@@ -1,23 +1,34 @@
-/*
-	Terms definition class
-*/
+/**
+  * values.scala - Definitions of different case classes to represent a value
+  * @author Lanvin Victor Thiré François
+  * Copyright (c) 2014 GPLv3. See LICENCE file
+  */
+
+
 abstract class Term() {
 	def toString : String
 }
 
-case class TermPair(t1 : Term, t2 : Term) extends Term {
-	val leftTerm = t1
-	val rightTerm = t2
+case class TermValue(v: Value) extends Term {
+  val value=v
 
-	override def toString : String = {
-		return "Pair(" + t1.toString() + "," + t2.toString() + ")"
-	}
+  override def toString : String = {
+    return v.toString
 }
 
 case class TermVariable(v : String) extends Term {
-	val id = v
+	val variable = v
 
 	override def toString : String = {
-		return id
+		return variable
+	}
+}
+
+case class TermPair(t1 : Term, t2 : Term) extends Term {
+	val leftTerm_m = t1
+	val rightTerm_m = t2
+
+	override def toString : String = {
+		return "Pair(" + leftTerm_m.toString() + "," + rightTerm_m.toString() + ")"
 	}
 }
