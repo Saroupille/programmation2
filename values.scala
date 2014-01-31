d4c6b655562789d1794612ac009f47caa9729986
VLanvin <victor.lanvin@ens-cachan.fr>
2014-01-31 12:25:31 +0100

    Fixed extractors

--- values.scala
+++ values.scala
@@ -3,13 +3,52 @@
 */
 
 abstract case class Value() extends Term {
-	
+
 }
 
-case class ValueInteger(v : Int) extends Value {
+// Extractor class for integer values
+class ValueInteger(v : Int) extends Value {
 	val value = v
 
 	override def toString : String = {
 		return value.toString
+	}
+}
+object ValueInteger {
+	def unapply(vi : ValueInteger): Option[Int] = Some(vi.value)
+}
+
+// Extractor class for constant values
+class ValueConst(s : String) extends Value {
+	val identifier = s
+
+	override def toString : String = {
+		return identifier
+	}
+}
+object ValueConst {
+	def unapply(vc : ValueConst): Option[String] = Some(vc.identifier)
+}
+
+
+//Some tests
+object Test extends App {
+	val p = new ValueInteger(10)
+	val p2 = new ValueConst("NomVal")
+	val p1 = new ValueInteger(15)
+
+	p2 match {
+		case ValueInteger(n) => println ("Valeur de p2 : " + n)
+		case ValueConst(str) => println ("Nom de p2 : " + str)
+	}
+
+	p match {
+		case ValueInteger(n) => println ("Valeur de p : " + n)
+		case ValueConst(str) => println ("Nom de p : " + str)
+	}
+
+	p1 match {
+		case ValueInteger(n) => println ("Valeur de p1 : " + n)
+		case ValueConst(str) => println ("Nom de p1 : " + str)
 	}
 }
