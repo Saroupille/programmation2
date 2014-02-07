5a220a9a8d5ee41a5aee7816fed52b761a9619e3
VLanvin <victor.lanvin@ens-cachan.fr>
2014-02-07 09:21:54 +0100

    Added parsing

---
+++ interpreter/parser.scala
@@ -0,0 +1,18 @@
+/**
+* parser.scala - Parser implementation
+* @author Lanvin Victor Thiré François
+* Copyright (c) 2014 GPLv3. See LICENCE file
+*/
+
+class Parser {
+
+	def parseProc(str : String) : Proc = {
+		if (str.startsWith("in(")) {
+
+		}
+		else if (str.startsWith("out(")) {
+			
+		}
+	}
+	
+}
