8669060c3bd41204b71d196f115ab036203a818f
Saroupille <franth2@gmail.com>
2014-02-15 12:50:49 +0100

    remove unecessary files

--- interpreter/exceptions.scala
+++
@@ -1,5 +0,0 @@
-case class noIntegerPublicKey(s: String) extends Exception(s)
-
-case class noIntegerSecretKey(s: String) extends Exception(s)
-
-case class parsingError(s: String) extends Exception(s)
