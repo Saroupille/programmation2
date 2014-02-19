e3a84bb0ced7610f68ec82038d37aa9d01f89003
Saroupille <franth2@gmail.com>
2014-02-19 08:45:59 +0100

    add some tests ont terms

--- built.sbt
+++ built.sbt
@@ -3,3 +3,5 @@ name := "Cryptit"
 version := "0.5"
 
 scalaVersion := "2.10.3"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.2.0"
--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,7 +1,24 @@
 object Test {
 
-  def interpretationTest() : Boolean = { 
-    println("coucou")
-    return true
+  def interpretationTest() : Unit = { 
+    println("###################")
+    println("Unit tests on terms")
+    println("###################")
+    val x= TermVariable("x")
+    println("Result expected : x")
+    println("Result          : "+x.interprete)
+    val y= TermVariable("y")
+    val pair1= TermPair(x,y)
+    println("Result expected : pair(x,y)")
+    println("Result          : "+pair1.interprete)
+    val proj1=TermProj1(pair1)
+    println("Result expected : x")
+    println("Result          : "+proj1.interprete)
+    val proj2=TermProj2(pair1)
+    println("Result expected : y")
+    println("Result          : "+proj2.interprete)
+    val list1=TermList(List(x,y,pair1,proj1,proj2))
+    println("Result expected : x::y::pair(x,y)::x::y::[]")
+    println("Result          : "+list1.interprete)
   }
 }
